afae03abd018c326f93af8a07f17e82dfc5c12fa
abdhulla-k <abdhullak149@gmail.com>
2022-05-15 16:46:02 +0530

    learned about inline styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  // we can add inline style. user styles insted of using styleUrls. Eg.
+  styles: [`
+     h1{
+       color: dodgerblue;
+     }
+  `]
+
 })
 export class AppComponent {
 }
