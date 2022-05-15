b148f7ee8e9c4ffbcfb9628660efaa3edd510261
abdhulla-k <abdhullak149@gmail.com>
2022-05-15 16:28:24 +0530

    learned how to add inline templates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //templateUrl: './servers.component.html',
+  // how to create in line template. user template insted of using tempateUrl. Eg.
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
