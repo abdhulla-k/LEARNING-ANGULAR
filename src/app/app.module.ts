87e2085751e6a4e3e6463515178cd86c24c2c556
abdhulla-k <abdhullak149@gmail.com>
2022-05-15 17:29:05 +0530

    created success-alert component. it is the first part of my first assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
