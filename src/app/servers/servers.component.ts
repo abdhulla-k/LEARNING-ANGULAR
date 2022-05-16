fbcdb2c9dbb3feb18d1a4b2c20451574294f3391
abdhulla-k <abdhullak149@gmail.com>
2022-05-16 19:21:29 +0530

    learned evend binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created';
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,10 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() :void {
+    this.serverCreationStatus = 'Server was Created!';
   }
 
 }
