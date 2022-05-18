9da4806eac3bcda0142ae28ee9f9dd5136486231
abdhulla-k <abdhullak149@gmail.com>
2022-05-18 20:57:45 +0530

    started an another work. deleted all the components to learn to create and use new and our own properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
-  // we can add inline style. user styles insted of using styleUrls. Eg.
-  styles: [`
-     h1{
-       color: dodgerblue;
-     }
-  `]
-
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
