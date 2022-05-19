660fc8472f99273d99095485c1e4679ede849e11
abdhulla-k <abdhullak149@gmail.com>
2022-05-19 22:32:43 +0530

    created tasks.

---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,30 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Doctors appointment',
+        day: 'May 5th at 2:30pm',
+        reminder: true,
+    },
+    
+    {
+        id: 2,
+        text: 'Meeting at school',
+        day: 'May 6th at 1:30pm',
+        reminder: true,
+    },
+    
+    {
+        id: 3,
+        text: 'learn angular',
+        day: 'May 7th at 2:30pm',
+        reminder: true,
+    },
+    
+    {
+        id: 4,
+        text: 'project meeting',
+        day: 'May 8th at 2:30pm',
+        reminder: true,
+    }
+]
