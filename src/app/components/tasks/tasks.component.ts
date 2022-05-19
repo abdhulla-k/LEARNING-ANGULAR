48445ccd50dc69ebca35c74090767c70835d5509
abdhulla-k <abdhullak149@gmail.com>
2022-05-19 22:56:06 +0530

    added tasks to task component. learned how to create and use dummy data

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task'
+import { TASKS } from '../../mock.tasks'
 
 @Component({
   selector: 'app-tasks',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksComponent implements OnInit {
 
+  tasks: Task[] = TASKS;
   constructor() { }
 
   ngOnInit(): void {
