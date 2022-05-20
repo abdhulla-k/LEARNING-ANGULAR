a35f8709f8986fe75a58c0e23a3d6bad56ef9e48
abdhulla-k <abdhullak149@gmail.com>
2022-05-20 19:25:44 +0530

    created functionality to delete task.

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -10,11 +10,14 @@ import { Task } from '../../Task'
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
 
-  constructor( private taskService: TaskService ) {
-    this.taskService.getTasks().subscribe(( tasks ) => this.tasks = tasks );
-  }
+  constructor( private taskService: TaskService ) {}
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe(( tasks ) => ( this.tasks = tasks ));
+  }
+
+  deleteTask( task:Task ) {
+    this.taskService.deleteTask( task ).subscribe(() => ( this.tasks = this.tasks.filter( t => t.id != task.id )))
   }
 
 }
