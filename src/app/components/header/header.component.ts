ffd92f0a7a866d27cb281130c834d232b6824d3c
abdhulla-k <abdhullak149@gmail.com>
2022-05-29 14:45:56 +0530

    created functionality to toggle the style of button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor( private uiService: UiService ) {
+    this.subscription = uiService
+    .onToggle()
+    .subscribe(( value ) => ( this.showAddTask = value ))
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log( 'togle' );
+    this.uiService.toggleAddTask();
   }
 
 }
