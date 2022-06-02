a108d2041cecc826cd48054530016e440e1fd5f7
abdhulla-k <abdhullak149@gmail.com>
2022-06-02 23:40:38 +0530

    learned about @ViewChiled decorator and how to access direct to the property in templeate by viewChiled decorator

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,7 +9,8 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter();
   @Output() blueprintCreated = new EventEmitter();
   //newServerName = '';
-  newServerContent = '';
+  //newServerContent = '';
+  @ViewChild( 'serverContentInput' ) serverDescription: ElementRef;
   
   constructor() { }
 
@@ -19,14 +20,14 @@ export class CockpitComponent implements OnInit {
   onAddServer( serverNameInput: HTMLInputElement ) {
     this.serverCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent
+      serverContent: this.serverDescription.nativeElement.value
     })
   }
 
   onAddBlueprint(blueprintNameInput: HTMLInputElement ) {
     this.blueprintCreated.emit({
       blueprintName: blueprintNameInput.value,
-      blueprintContent: this.newServerContent
+      blueprintContent: this.serverDescription.nativeElement.value
     })
   }
 
