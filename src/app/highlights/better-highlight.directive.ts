7a90eb9845ab7a8776f9275f00391c3c6243a1c9
abdhulla-k <abdhullak149@gmail.com>
2022-06-04 21:33:52 +0530

    learned about custome data binding

--- src/app/highlights/better-highlight.directive.ts
+++ src/app/highlights/better-highlight.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2
 } from '@angular/core';
@@ -11,21 +12,24 @@ import {
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding( 'style.backgroundColor' ) backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = 'transperent';
+  @Input() highlightColor: string = 'blue';
+  @HostBinding( 'style.backgroundColor' ) backgroundColor: string /* = 'transparent'; */
 
   constructor( private elRef: ElementRef, private renderer: Renderer2 ) { }
 
   ngOnInit(): void {
       //this.renderer.setStyle( this.elRef.nativeElement, 'backgroundColor', 'blue' )
+      this.backgroundColor = this.defaultColor;
   }
 
   @HostListener( 'mouseenter' ) mouseover( eventData: Event ) {
     //this.renderer.setStyle( this.elRef.nativeElement, 'backgroundColor', 'blue' );
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener( 'mouseleave' ) mouseleave( eventData: Event ) {
     // this.renderer.setStyle( this.elRef.nativeElement, 'backgroundColor', 'yellow' );
-    this.backgroundColor = 'yellow'
+    this.backgroundColor = this.defaultColor;
   }
 }
