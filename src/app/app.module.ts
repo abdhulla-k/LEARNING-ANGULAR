de3f1be0b46bbe64f23e376eb20c5df4da5a58ca
abdhulla-k <abdhullak149@gmail.com>
2022-06-04 09:08:36 +0530

    created three new components game-controal, even and odd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
     ServerComponent,
     ServersComponent,
     SuccessAlertComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/odd/odd.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-odd-componet',
+    templateUrl: "./odd.component.html",
+    styleUrls: ['./odd.component.css']
+})
+export class OddComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void {
+        
+    }
+}
