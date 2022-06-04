007619995e8b7e372da00bb5d1332bd76ea1fc51
abdhulla-k <abdhullak149@gmail.com>
2022-06-04 13:03:57 +0530

    created a feature to show either recipe component or shopping list component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,11 @@ import { Component } from "@angular/core";
     styleUrls: []
 })
 export class HeaderComponent {
+    @Output() statusOutput = new EventEmitter<string>();
+    status: string;
 
+    onSelect( option: string ) {
+        this.status = option;
+        this.statusOutput.emit( this.status );
+    }
 }
