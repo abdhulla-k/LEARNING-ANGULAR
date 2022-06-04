164e62d63db454d7ca7a0b879cfffb124b948d52
abdhulla-k <abdhullak149@gmail.com>
2022-06-04 15:34:45 +0530

    created feture to add new ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  EventEmitter,
+  Output 
+} from '@angular/core';
+import { Incredient } from 'src/app/shared/incredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +14,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild( 'nameInput' ) nameInputRef: ElementRef;
+  @ViewChild( 'amountInput' ) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Incredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Incredient( ingName, ingAmount );
+    this.ingredientAdded.emit( newIngredient );
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded( ingredient: Incredient ) {
+    this.incredients.push( ingredient );
+  }
+
 }
