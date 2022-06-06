b3d06f08882c857d3fd570a6eb0dfa694cf84ab9
abdhulla-k <abdhullak149@gmail.com>
2022-06-06 15:45:21 +0530

    added ingreedient field to recipe model. added a new recipe in service. added the ingreedient data to the recipe-detail

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Incredient } from "../shared/incredient.model";
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredient: Incredient[];
 
-    constructor( name: string, description: string, imagePath: string ) {
+    constructor( name: string, description: string, imagePath: string, ingredient: Incredient[] ) {
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
+        this.ingredient = ingredient;
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,29 @@
 import { EventEmitter } from "@angular/core";
+import { Incredient } from "../shared/incredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe( 'A test recipe', 'This is siply a test', 'https://picturetherecipe.com/wp-content/uploads/2020/01/Rogan-Josh-by-PictureTheRecipe-Featured-1-395x500.jpg')
+        new Recipe( 
+            'sandwich',
+            'chicken sandwich',
+            'https://www.chicken.ca/wp-content/uploads/2020/09/Chicken-Fine-Herb-Mayo.jpg',
+            [
+                new Incredient( 'buns', 2 ),
+                new Incredient( 'chicken', 100 )
+            ]
+        ),
+        new Recipe(
+            'Burger',
+            'what else you need to say?',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Hamburger_%28black_bg%29.jpg/1200px-Hamburger_%28black_bg%29.jpg',
+            [
+                new Incredient( 'buns', 2 ),
+                new Incredient( 'Meet', 1 )
+            ]
+        )
     ];
 
     getRecipes() {
