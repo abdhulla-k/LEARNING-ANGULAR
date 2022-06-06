7c5f48ba91fbaf5b051554e186df3881d03e83bd
abdhulla-k <abdhullak149@gmail.com>
2022-06-06 14:15:43 +0530

    moved all ingredients to shopping-list-service and made this data available in components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Incredient } from '../shared/incredient.model';
 
+import { ShoppingListService } from './shopping-list.service';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ ShoppingListService ]
 })
 export class ShoppingListComponent implements OnInit {
-  incredients: Incredient[] = [
-    new Incredient( 'Apple', 5 ),
-    new Incredient( 'Tomate', 10 )
-  ];
-  constructor() { }
+  // incredients: Incredient[] = [
+  //   new Incredient( 'Apple', 5 ),
+  //   new Incredient( 'Tomate', 10 )
+  // ];
+
+  incredients: Incredient[] = [];
+  constructor(  private shoppingListService: ShoppingListService ) {}
 
   ngOnInit(): void {
+    this.incredients = this.shoppingListService.getIngredients();
   }
 
   onIngredientAdded( ingredient: Incredient ) {
-    this.incredients.push( ingredient );
+    this.shoppingListService.ingredientAdded( ingredient );
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Incredient } from "../shared/incredient.model";
+
 export class ShoppingListService {
-    
+    private incredients: Incredient[] = [
+        new Incredient( 'Apple', 5 ),
+        new Incredient( 'Tomate', 10 )
+    ];
+
+    getIngredients() {
+        return this.incredients.slice(); // slice method will return a copy of incredients
+    }
+
+    ingredientAdded( ingredient: Incredient ) {
+        this.incredients.push( ingredient );
+    }
 }
