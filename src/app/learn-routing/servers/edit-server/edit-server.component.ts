f13617da9b38b05544051406841c03aeebd91d45
abdhulla-k <abdhullak149@gmail.com>
2022-06-07 16:16:05 +0530

    learned how to retrieve the queryParams and fragment

--- src/app/learn-routing/servers/edit-server/edit-server.component.ts
+++ src/app/learn-routing/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +13,15 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute ) {
+
+    }
 
   ngOnInit() {
+    console.log( this.route.snapshot.fragment );
+    console.log( this.route.snapshot.queryParams );
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
