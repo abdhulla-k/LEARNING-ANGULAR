f753d1505f49ad2e4e6cd6825383eb11e4d18426
abdhulla-k <abdhullak149@gmail.com>
2022-06-10 16:31:18 +0530

    learned about operators and subjects in RxJS

--- src/app/learn-observables/home/home.component.ts
+++ src/app/learn-observables/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { observable, Observable, Subscription } from 'rxjs';
+import { observable, Observable, Subscription, of } from 'rxjs';
+import { map, reduce, filter } from 'rxjs/operators' 
 
 @Component({
   selector: 'app-home',
@@ -13,27 +14,34 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnInit() {
 
     // this is how to create an observable
-    const customIntervalObservable = Observable.create(observer => {
-      let count = 0;
-      setInterval( () => {
-        observer.next(count);
-        if( count === 3 ) {
-          observer.complete()
-        }
-        // if( count > 3 ) {
-        //   observer.error( new Error( 'count is greater thathan 3' ))
-        // }
-        count++;
-      }, 1000 );
-    })
+    // const customIntervalObservable = Observable<any>.create(observer => {
+    //   let count = 0;
+    //   setInterval( () => {
+    //     observer.next(count);
+    //     if( count === 3 ) {
+    //       observer.complete()
+    //     }
+    //     // if( count > 3 ) {
+    //     //   observer.error( new Error( 'count is greater thathan 3' ))
+    //     // }
+    //     count++;
+    //   }, 1000 );
+    // })
 
-    this.firstObsSubsctiption = customIntervalObservable.subscribe( data => {
-      console.log(data);
-    })
+    // this.firstObsSubsctiption = customIntervalObservable.subscribe( data => {
+    //   console.log(data);
+    // })
+
+    let test1 = of( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 );
+    let case1 = test1.pipe(
+      filter( x => x % 2 == 0 ),
+      reduce(( acc,one ) => acc + one , 0 )
+    )
+    case1.subscribe( x => console.log(x));
   }
 
   ngOnDestroy(): void {
-      this.firstObsSubsctiption.unsubscribe()
+      // this.firstObsSubsctiption.unsubscribe()
   }
 
 }
