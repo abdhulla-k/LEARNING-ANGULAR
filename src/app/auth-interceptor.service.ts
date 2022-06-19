7a087671745dfcd5ae806df5fe9e863597e02ab5
abdhulla-k <abdhullak149@gmail.com>
2022-06-19 10:21:27 +0530

    learned how to modify the http respons using interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,8 +1,15 @@
-import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs";
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept( req: HttpRequest<any>, next: HttpHandler ) {
         let modifiedReq = req.clone({ headers: req.headers.append( 'auth', 'xyz' )} )
-        return next.handle( modifiedReq );
+        return next.handle( modifiedReq ).pipe( tap( event => {
+            console.log( event );
+            if( event.type === HttpEventType.Response ) {
+                console.log( 'response arrived boady data: ' );
+                console.log( event.body );
+            }
+        }));
     }
 }
