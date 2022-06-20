4ed72029185aaab9e35936b97732e1ee349d4024
abdhulla-k <abdhullak149@gmail.com>
2022-06-20 17:41:42 +0530

    created auto logout method to logout when the token get expired

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,12 +19,26 @@ export interface AuthResponseData {
 })
 export class AuthService {
     user = new BehaviorSubject<User>(null); // behaviour subject is also subject. But this give us the previous data
+    private tokenExpirationTimer: any;
     constructor( private http: HttpClient, private router: Router ) {}
 
     logout() {
         this.user.next( null );
         this.router.navigate( ['/auth'] )
+        localStorage.removeItem( 'userData' );
+        
+        if( this.tokenExpirationTimer ) {
+            clearTimeout( this.tokenExpirationTimer );
+        }
+        this.tokenExpirationTimer = null;
     }
+
+    autoLogout( expirationDuration: number ) {
+
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration )
+    } 
 
     signUp( email: string, password: string ) {
         return this.http.post<AuthResponseData>( 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCYwMrOgop4QDlgTsgFwA9i3RXHoHWbdrk',
@@ -70,6 +84,8 @@ export class AuthService {
 
         if( loadUser.token ) {
             this.user.next( loadUser );
+            const expirationDuration = new Date( userData._tokenExpirationDate ).getTime() - new Date().getTime()
+            this.autoLogout( expirationDuration );
         }
     }
 
@@ -111,6 +127,7 @@ export class AuthService {
             expirationDate
         )
         this.user.next( user )
+        this.autoLogout( expiresIn * 1000 )
         localStorage.setItem( 'userData', JSON.stringify( user ));
     }
 
