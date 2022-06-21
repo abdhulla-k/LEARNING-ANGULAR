f18a71ef063d51698935761d93711f6dc886ee61
abdhulla-k <abdhullak149@gmail.com>
2022-06-21 14:03:09 +0530

    learned how to create dinamic component using DynamiComponentLoder

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,29 @@
-import { Component, ViewChild } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 
 import { AuthService, AuthResponseData } from "./auth.service";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
     isLoginMode = true;
     isLoading = false;
     error = null;
+    @ViewChild( PlaceholderDirective ) alertHost: PlaceholderDirective
 
-    constructor( private authService: AuthService, private router: Router ) {}
+    private closeSub: Subscription
+
+    constructor( 
+        private authService: AuthService, 
+        private router: Router,
+        private componetFactoryResolver: ComponentFactoryResolver
+    ) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -49,6 +58,7 @@ export class AuthComponent {
             error => {
                 this.isLoading = false;
                 this.error = error;
+                this.showErrorAlert( error );
                 console.log(error)
             }
         )
@@ -58,5 +68,28 @@ export class AuthComponent {
 
     onHandleError() {
         this.error = null;
+    }
+
+    private showErrorAlert( error: string ) {
+        const alertComponentFactory = this.componetFactoryResolver.resolveComponentFactory(
+            AlertComponent 
+        );
+
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear()
+        const componentRef = hostViewContainerRef.createComponent( alertComponentFactory );
+
+
+        componentRef.instance.message = error;
+        this.closeSub = componentRef.instance.errorHandled.subscribe(() => {
+            this.closeSub.unsubscribe();
+            hostViewContainerRef.clear();
+        })
+    }
+
+    ngOnDestroy(): void {
+        if( this.closeSub ) {
+            this.closeSub.unsubscribe();
+        }
     }
 }
