9da794d3f86c484435986672a0ad880ad1e68628
abdhulla-k <abdhullak149@gmail.com>
2022-06-22 14:10:06 +0530

    created recipe  module to split recipe related activities from app module. learned how to create seperate modules

---
+++ src/app/recipes.module.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeStartComponent
+    ],
+    imports:[
+        CommonModule,
+        ReactiveFormsModule,
+        RouterModule
+    ],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeEditComponent,
+        RecipeStartComponent
+    ]
+})
+export class RecipesModule {
+
+}
